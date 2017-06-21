b3c8839e949e0b51912929a26e71a553ecb3a6a6
Dean <dean.wu55@gmail.com>
2017-06-21 17:20:50 -0400

    lol

--- js/script.js
+++ js/script.js
@@ -2,55 +2,59 @@ var app = {}
 
 app.init = function() {
 	app.addNavScrollListener();
-	app.addSkillsSelectorListener();
 	app.heroAnimation();
 }
 
 app.heroAnimation = function() {
 	if ($(window).width() > 720) {
-		app.renderLetter("DEAN WU", 0); 
+		app.renderHero("DEAN WU", 0); 
+		app.windowResizeHeroListener();
 	}
 	else {
-		$(".hero h1").html("DW");
+		app.heroResize();
+		$(".hero h2").css("transform", "none");
+		$(".hero h2").css("opacity", "1.0")
+		app.windowResizeHeroListener();
 	}
 }
 
-app.renderLetter = function(str, index) {
-	var alphabetValue = str.charCodeAt(index)-64;
-	var speed = 40+(500/alphabetValue*0.6);
+app.heroResize = function() {
+	if ($(window).width() < 320) {
+		$("h1").html("DW");
+		$("h1").css("font-size", "100px");
+	}
+	else {
+		$("h1").html("DEAN WU");
+	}
+}
+
+app.windowResizeHeroListener = function() {
+	$(window).resize(function () {
+		console.log("resizing");
+		app.heroResize();
+	});
+}
+
+app.renderHero = function(str, index) {
+	var alphabetIndex = str.charCodeAt(index)-64;
+	var speed = 40+(500/alphabetIndex*0.6);
 	if (index >= str.length) {
-		$(".hero h1 span").remove();
+		$("h1 span").remove();
+		$(".hero h2").css("transform", "none");
+		$(".hero h2").css("opacity", "1.0")
 		return;
 	}
 	var i = 65;
 	var intervalId = setInterval(function() {
 		var temp = String.fromCharCode(i);
-		$(".hero h1 span").html(temp);
+		$("h1 span").html(temp);
 		if (temp == str[index] || i >= 90 || str[index] == " ") {
-			$(".hero h1").html(`${str.slice(0, index+1)}<span></span>`);
+			$("h1").html(`${str.slice(0, index+1)}<span></span>`);
 			clearInterval(intervalId);
-			app.renderLetter(str, index+1);
+			app.renderHero(str, index+1);
 		}
 		i++;
 	}, speed);
-}
-
-app.addSkillsSelectorListener = function() {
-	$(".skills-selector .btn").on("click", function() {
-		$(".skills-selector .btn").removeClass("btn--selected");
-		$(this).addClass("btn--selected");
-		switch($(this).html()) {
-			case "Proficient":
-				$(".skills-list > *").css("left", "-0%");
-				break;
-			case "Experienced":
-				$(".skills-list > *").css("left", "-33.33%");
-				break;
-			case "Beginner":
-				$(".skills-list > *").css("left", "-66.66%");
-				break;
-		}
-	})
 }
 
 app.addNavScrollListener = function() {
